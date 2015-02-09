a2e89d1fd7a82ddfdc1ee75bfe6cfb07b88b2bd8
Daewju <the.dawju@gmail.com>
2015-02-09 20:09:29 +0100

    sdfd

--- EasyLearning/src/sp/Gui.java
+++ EasyLearning/src/sp/Gui.java
@@ -1,4 +1,5 @@
 package sp;
+
 //halloo
 
 import java.awt.event.ActionEvent;
@@ -8,66 +9,99 @@ import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
-public class Gui extends JFrame{
-	
+public class Gui extends JFrame
+{
 
-	public Gui(){
-		setLayout(null); //LayoutManager verwenden
+	public Gui()
+	{
+		setLayout(null); // LayoutManager verwenden
 		setVisible(true);
-		setSize(800,600);
+		setSize(800, 600);
 		setTitle("EasyLearning");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		menuezeileErzeugen(this);
 		repaint();
-	
+		bestaetigungsDialog(this, "Irgndetwas hat einen Fehler!");
 	}
-	
-	
-	public void menuezeileErzeugen(JFrame gui){
-		
+
+	public void menuezeileErzeugen(JFrame gui)
+	{
 		JMenuBar menuezeile = new JMenuBar();
 		gui.setJMenuBar(menuezeile);
-		
+
 		JMenu dateiMenue = new JMenu("Datei");
 		menuezeile.add(dateiMenue);
-		
-		JMenuItem neueKarteiEintrag = new JMenuItem("Neue Kartei");
-		neueKarteiEintrag.addActionListener(new ActionListener(){public void actionPerformed(ActionEvent e){test();}});
+
+		JMenuItem neueKarteiEintrag = new JMenuItem("Neu");
+		neueKarteiEintrag.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				//toDo
+			}
+		});
 		dateiMenue.add(neueKarteiEintrag);
 
-		JMenuItem oeffnenEintrag = new JMenuItem("Ã–ffnen");
-	//	oeffnenEintrag.addActionListener((ActionListener) this);
+		JMenuItem oeffnenEintrag = new JMenuItem("Öffnen");
+		oeffnenEintrag.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				//toDo
+			}
+		});
 		dateiMenue.add(oeffnenEintrag);
-		
+
 		JMenuItem spracheEintrag = new JMenuItem("Sprache");
-	//	spracheEintrag.addActionListener((ActionListener) this);
+		spracheEintrag.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				//toDo
+			}
+		});
 		dateiMenue.add(spracheEintrag);
 
-		
 		JMenuItem beendenEintrag = new JMenuItem("Beenden");
-	//	beendenEintrag.addActionListener((ActionListener) this);
+		beendenEintrag.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				//toDo
+			}
+		});
 		dateiMenue.add(beendenEintrag);
-		
-		
 	}
 	
-	public void test()
+	public void warnungsDialog(Gui gui, String text)
 	{
-		setSize(1600,600);
-		repaint();
+		JOptionPane.showMessageDialog(gui,
+			    text,
+			    "Warnung",
+			    JOptionPane.WARNING_MESSAGE);
 	}
 	
+	public void fehlerDialog(Gui gui, String text)
+	{
+		JOptionPane.showMessageDialog(gui,
+			    text,
+			    "Fehler",
+			    JOptionPane.ERROR_MESSAGE);
+	}
 	
-	public static void main(String[] args) {
+	public void bestaetigungsDialog(Gui gui, String text)
+	{
+		JOptionPane.showInternalConfirmDialog(gui,
+			    text,
+			    "Fehler",
+			    JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	public static void main(String[] args)
+	{
 		JFrame gui = new Gui();
-		
 
 	}
-	
-	class neueKarteiActionsListener implements ActionsListener
-	{
-		
-	}
-
 }
