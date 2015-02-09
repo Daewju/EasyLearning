0abb4bc0902969037acbddc41987373edadb9829
Daewju <the.dawju@gmail.com>
2015-02-09 19:37:56 +0100

    ...

--- EasyLearning/src/sp/Gui.java
+++ EasyLearning/src/sp/Gui.java
@@ -1,10 +1,10 @@
 package sp;
 //halloo
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -13,12 +13,13 @@ public class Gui extends JFrame{
 	
 
 	public Gui(){
-		setLayout(null);
+		setLayout(null); //LayoutManager verwenden
 		setVisible(true);
 		setSize(800,600);
 		setTitle("EasyLearning");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		menuezeileErzeugen(this);
+		repaint();
 	
 	}
 	
@@ -31,23 +32,29 @@ public class Gui extends JFrame{
 		menuezeile.add(dateiMenue);
 		
 		JMenuItem neueKarteiEintrag = new JMenuItem("Neue Kartei");
-		neueKarteiEintrag.addActionListener((ActionListener) this);
+		neueKarteiEintrag.addActionListener(new ActionListener(){public void actionPerformed(ActionEvent e){test();}});
 		dateiMenue.add(neueKarteiEintrag);
 
 		JMenuItem oeffnenEintrag = new JMenuItem("Öffnen");
-		oeffnenEintrag.addActionListener((ActionListener) this);
+	//	oeffnenEintrag.addActionListener((ActionListener) this);
 		dateiMenue.add(oeffnenEintrag);
 		
 		JMenuItem spracheEintrag = new JMenuItem("Sprache");
-		spracheEintrag.addActionListener((ActionListener) this);
+	//	spracheEintrag.addActionListener((ActionListener) this);
 		dateiMenue.add(spracheEintrag);
 
 		
 		JMenuItem beendenEintrag = new JMenuItem("Beenden");
-		beendenEintrag.addActionListener((ActionListener) this);
+	//	beendenEintrag.addActionListener((ActionListener) this);
 		dateiMenue.add(beendenEintrag);
 		
 		
+	}
+	
+	public void test()
+	{
+		setSize(1600,600);
+		repaint();
 	}
 	
 	
@@ -55,6 +62,11 @@ public class Gui extends JFrame{
 		JFrame gui = new Gui();
 		
 
+	}
+	
+	class neueKarteiActionsListener implements ActionsListener
+	{
+		
 	}
 
 }
