7f445e540a86596260a177c978887ba523683ca3
Daewju <the.dawju@gmail.com>
2015-02-24 18:41:07 +0100

    ...

---
+++ EasyLearning/src/mk/EasyL.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package mk;
+
+import sp.GuiMain;
+
+
+
+/**
+ * @author marko
+ *
+ */
+public class EasyL {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final GuiMain gui = new GuiMain();
+
+	}
+
+}
